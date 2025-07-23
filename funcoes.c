b914f36e2591ef5fe2720e5b1eff80efc46acd9c
Ana Carolina Morone <83526857+anamorone@users.noreply.github.com>
2025-07-23 17:23:05 -0300

    funcao 4

--- funcoes.c
+++ funcoes.c
@@ -81,3 +81,39 @@ void mostrarCliente(Cliente c) { //Ana
     printf("\nNome: %s\nCPF: %s\nNascimento: %s\nTelefone: %s\nEmail: %s\n",
            c.nome, c.cpf, c.nascimento, c.telefone, c.email);
 }// Mostra as informações do cliente validado
+
+// FUNCAO ANIVERSARIANTES
+
+void aniversariantesMes(Cliente agenda[], int total) { //Ana
+    int mes;
+    
+    printf("\nDigite o mês (1-12): ");
+    scanf("%d", &mes); getchar(); // Digitar o mês desejado para visualização
+    
+    if (mes < 1 || mes > 12) {
+        printf("Mês inválido.\n");
+        return;
+    } // Mensagem de mês inválido
+
+    int indices[MAX_CLIENTES], count = 0;
+    for (int i = 0; i < total; i++) {
+        if (extrairMes(agenda[i].nascimento) == mes) {
+            indices[count++] = i;
+        }
+    } // Os aniversariantes tem índices armazenados no vetor e suas quantidades são contadas
+
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (strcmp(agenda[indices[i]].nascimento, agenda[indices[j]].nascimento) > 0) {
+                int temp = indices[i];
+                indices[i] = indices[j];
+                indices[j] = temp;
+            }
+        }
+    } // Bubble Sort para comparar as datas de nascimento dos aniversariantes de forma crescente
+
+    printf("\nAniversariantes do mês %d:\n", mes);
+    for (int i = 0; i < count; i++) {
+        mostrarCliente(agenda[indices[i]]);
+    } // Mostra os aniversariantes do mês selecionado
+}
